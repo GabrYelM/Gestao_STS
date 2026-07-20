a0e7f8c5336eaff6d354981a5aa31ee672f22a85
gabryel gomes martins <gabryel254@gmail.com>
2026-07-20 12:34:05 -0300

    commit 4: função de download implementada, finalizada a função de coleta do AG04

--- services/bot.py
+++ services/bot.py
@@ -12,73 +12,92 @@ def bot_setup_page():
     from playwright.sync_api import sync_playwright
     p = sync_playwright().start()
 
-    browser = p.chromium.launch(headless=False, channel="msedge", slow_mo=100)
-    context = browser.new_context(http_credentials={'username': usuario, 'password': senha})
+    browser = p.chromium.launch(headless=False, channel="msedge", slow_mo=3000)
+    context = browser.new_context(http_credentials={'username': usuario, 'password': senha}, accept_downloads=True)
     page = context.new_page()
+
+    click_timeout = 0
+    timeout_geral = 0
 
     print("-------Login com sucesso-------")
     return p, browser, page
 
-def buscaAG04(mes, ano, page):
-    # Pelo o que pesquisei, seria mehor nao usar o page.wait_for_load_state("networkidle") porque ela poderia
-    # deixar lento o código já que as funções já esperam automaticamente o carregamento da página.
-
-    # Usei o Enter para ir para a próxima etapa, tudo funciona bem até clicar em "Aplicar", ele clica automaticamente por causa do "Enter"
-    # Ele fica "Carregando" infinitamente e não carrega o relatório, não alterei mais nada, só fiquei rodando, e do nada tudo parou de funcionar, nem a página carregava mais
-    # Acho que deve ser por acessar muitas vezes a página, deve voltar ao normal mais tarde.
+def buscaAG04(mes, ano, page, click_timeout=1000, timeout_geral=1000):
     print("Coletando relatório mensal...")
-    page.goto("http://bisaude.prodam/sites/siga/Relatrio/Forms/current.aspx")
+
+    page.goto("https://biprodam.saude.prefeitura.sp.gov.br/sites/siga/Paginas/Inicial.aspx")
     page.get_by_text("Agendamentos").click()
 
     page.get_by_text("AG-04 Perda Secundária por Executante").click()
 
+    page.wait_for_timeout(timeout_geral)
     page.get_by_title("Parâmetro de relatório Data Agendada.Número Ano").click()
-    page.get_by_text(ano).click(timeout=5000)
-    page.keyboard.press("Enter")
+    page.get_by_text(ano).click(timeout=click_timeout)
+    page.locator("#m_sqlRsWebPart_ctl00_ctl19_ButtonCell").click(position={"x": 10, "y": 10}, timeout=click_timeout)
 
+    page.wait_for_timeout(timeout_geral)
     page.get_by_title("Parâmetro de relatório Data Agendada.Nome Mes").click()
-    page.get_by_text(mes).click(timeout=5000)
-    page.keyboard.press("Enter")
+    page.get_by_text(mes).click(timeout=click_timeout)
+    page.locator("#m_sqlRsWebPart_ctl00_ctl19_ButtonCell").click(position={"x": 10, "y": 10}, timeout=click_timeout)
     
+    page.wait_for_timeout(timeout_geral)
     page.get_by_title("Parâmetro de relatório Estabelecimento de Saúde do Executante.H1 - Nome Nível 2").click()
-    page.get_by_text("COORD REGIONAL DE SAUDE SUDESTE").click(timeout=5000)
-    page.keyboard.press("Enter")
+    page.get_by_text("COORD REGIONAL DE SAUDE SUDESTE").click(timeout=click_timeout)
+    page.locator("#m_sqlRsWebPart_ctl00_ctl19_ButtonCell").click(position={"x": 10, "y": 10}, timeout=click_timeout)
 
+    page.wait_for_timeout(timeout_geral)
     page.get_by_title("Parâmetro de relatório Estabelecimento de Saúde do Executante.H1 - Nome Nível 3").click()
-    page.get_by_text("SUDESTE - STS PENHA").click()
-    page.get_by_text("All").click()
-    page.keyboard.press("Enter")
+    page.get_by_text("SUDESTE - STS PENHA").click(timeout=click_timeout)
+    page.get_by_text("All").click(timeout=click_timeout)
+    page.locator("#m_sqlRsWebPart_ctl00_ctl19_ButtonCell").click(position={"x": 10, "y": 10}, timeout=click_timeout)
 
+    page.wait_for_timeout(timeout_geral)
     page.get_by_title("Parâmetro de relatório Digite $$ para todos ou parte do nome do Estabelecimento para pesquisa").fill("$$")
-    page.keyboard.press("Enter")
+    page.locator("#m_sqlRsWebPart_ctl00_ctl19_ButtonCell").click(position={"x": 10, "y": 10}, timeout=click_timeout)
 
+    page.wait_for_timeout(timeout_geral)
     page.get_by_title("Parâmetro de relatório Estabelecimento de Saúde do Executante.H1 - Nome Estabelecimento").click()
-    page.get_by_text("All", exact=True).click(timeout=5000)
-    page.keyboard.press("Enter")
+    page.get_by_text("All", exact=True).click(timeout=click_timeout)
+    page.locator("#m_sqlRsWebPart_ctl00_ctl19_ButtonCell").click(position={"x": 10, "y": 10}, timeout=click_timeout)
 
-    
+    page.wait_for_timeout(timeout_geral)
     page.get_by_title("Parâmetro de relatório Digite $$ para todos ou parte do nome do procedimento para pesquisa").fill("$$")
-    page.keyboard.press("Enter")
+    page.locator("#m_sqlRsWebPart_ctl00_ctl19_ButtonCell").click(position={"x": 10, "y": 10}, timeout=click_timeout)
 
+    page.wait_for_timeout(timeout_geral)
     page.get_by_title("Parâmetro de relatório Procedimento.Nome Procedimento").click()
-    page.get_by_text("All", exact=True).click(timeout=5000)
-    page.keyboard.press("Enter")
+    page.get_by_text("All", exact=True).click(timeout=click_timeout)
+    page.locator("#m_sqlRsWebPart_ctl00_ctl19_ButtonCell").click(position={"x": 10, "y": 10}, timeout=click_timeout)
 
-    page.wait_for_load_state("networkidle")
+    page.wait_for_timeout(timeout_geral)
+    page.locator("input[value='Aplicar']").click(timeout=click_timeout)
 
-    """ page.get_by_text("Ações").click()
-    page.get_by_text("Exportar").click() """
+    page.wait_for_load_state('networkidle')
+    page.get_by_text("Ações").first.click(timeout=click_timeout)
+    page.get_by_text("Exportar").click(timeout=click_timeout)
+
+    download_bi(page)
 
     input("pressione enter no terminal para finalizar")
     
+def download_bi(page, click_timeout=1000, timeout_geral=1000):
+    with page.expect_download() as info_download:
+        page.get_by_text("CSV ponto e vírgula").click(timeout=click_timeout)
+
+    download = info_download.value
+    nome_original = download.suggested_filename
+
+    save_path = fr"C:\.Projetos\Gestao_STS\ARQUIVOS ORIGINAIS\{nome_original}"
+
+    os.makedirs("C:\.Projetos\Gestao_STS\ARQUIVOS ORIGINAIS", exist_ok=True)
+    download.save_as(save_path)
 
 if __name__ == '__main__':
-    p, browser, page = bot_setup_page()
+    p, context, page = bot_setup_page()
 
     try:
         buscaAG04("Janeiro", "2026", page)
-
     finally:
         print("-------Fechando-------")
-        browser.close()
+        context.close()
         p.stop()
